4ee756bdd3c33ab02a343faaa07004da2bd8e7ff
Bruno Lima <sdzephyrx@gmail.com>
2023-10-06 08:56:22 -0300

    criacao do serialize e configuracao das rotas

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,14 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1, 'nome':'Bruno'}
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos(as)"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
